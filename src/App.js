c5060aecce216318135b147a762505b61cb3559d
luis <lazamora77@misena.edu.co>
2019-02-22 16:12:45 -0500

    react

--- src/App.js
+++ src/App.js
@@ -2,27 +2,105 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {todos} from './todos.json'; 
+
+import TodoForm from './componentes/TodoForm';
+
+
+
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+
+    this.agregaUnaTarea = this.agregaUnaTarea.bind(this);   
+
+  }
+
+  //eliminar card//
+
+  removeTodo(index){
+    
+    this.setState({
+      todos: this.state.todos.filder((e,i) => {
+        return i !== index  
+      })
+  
+    });
+  }
+
+
+  // metodo encargado de agrgar nuevos datos a las tareas //
+
+  agregaUnaTarea(todo) {
+    console.log(todo);
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  // encargado de agregar  nuevos datos a las tareas //  
+
+render(){
+  const todos = this.state.todos.map((todo, i) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className='col-md-4' key={i}> 
+        <div className='col-mt-4'>
+          <div className='card-decks'>
+            <div className='card mt-4'>
+              <div className='card-header'>
+                <h3> {todo.title} </h3>
+              
+                <span className='badge badge-pill badge-danger ml-2'>
+                  {todo.priority}
+                </span>
+              </div>
+              <div className='card-body'>
+                <p> {todo.Description } </p>
+                <p> {todo.responsible} </p>
+              <div className='card-footer'>
+                <button className='btn btn-danger' onClick={this.removeTodo.bind(this, i)}>
+                  Eliminar
+                </button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    )
+  });
+
+// datos de para las cards extraidas de 'todos.json' //  
+
+    return (
+      <div className="App"> 
+        <nav className='container navbar navbar-dark bg-dark '>
+          <a href="#" className="text-white">
+            Tareas
+            <span className='badge badge-pill badge-light ml-2'>
+              { this.state.todos.length }
+            </span>
+          </a>  
+            <img src={logo} className="App-logo  d-flex justify-content-end " alt="logo"  />
+                   
+        </nav>
+        <div className='container'>
+          <div className='row md-4'>
+            <div className='col-md-4 text-center'>
+              <TodoForm  onAddTodo = { this.agregaUnaTarea }></TodoForm>
+            </div>            
+            {todos}
+          </div>
+        </div>
+    
+       
+      </div>  
     );
   }
 }
 
 export default App;
+ 
---
+++ src/componentes/TodoForm.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+
+
+class TodoForm extends Component {
+    constructor (){
+        super ();
+        this.state = {
+            title :'',
+            responsible:'',
+            Description :'',
+            priority :''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+   
+    }
+
+    // Enlazando  //
+
+    handleSubmit(e) {
+        e.prevenDefault()
+        this.props.onAddTodo(this.state);
+        this.setState({
+            title :'',
+            responsible:'',
+            Description :'',
+            priority :''
+
+        });  
+        
+    }
+
+    handleInputChange(e){
+        const {value,name }=e.target;
+        console.log (value,name);
+        this.setState({
+            [name]:value
+
+        });
+    }
+
+    // formulario //  
+
+    render () {   
+        return (
+            <div className = 'card mt-4'>
+                <form className='card-body' onSubmit={this.handleSubmit}>
+                    <div className='form-group'>
+                        <input 
+                            type ='text'
+                            name='title'
+                            className='form-control'
+                            value={this.state.title}
+                            onChange={this.handleInputChange}
+                            placeholder='Title'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type ='text'
+                            name='responsible'
+                            className='form-control'
+                            value={this.state.responsible}
+                            onChange={this.handleInputChange}
+                            placeholder='Responsible'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input 
+                            type ='text'
+                            name='Description'
+                            className='form-control'
+                            value={this.state.Description}
+                            onChange={this.handleInputChange}
+                            placeholder='Description'
+
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <select name='priority' className='form-control'  type ='text'
+                            name='priority'
+                            className='form-control'
+                            placeholder='priority'
+                            value={this.state.priority}
+                            onChange={this.handleInputChange}>
+                            
+                            <option>low</option>
+                            <option>medium</option>
+                            <option>high</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="btn btn-primary" >
+                        Agregar
+                    </button>
+                </form>
+            </div> 
+                    
+        )
+
+    }
+    
+}
+
+export default TodoForm; 
